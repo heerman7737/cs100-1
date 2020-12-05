eaffb432c848474a24e74f43c8dfe28a990f57fd
rrchandler <ryanchandler50@gmail.com>
2020-12-05 12:29:34 -0800

    Added more majors for display everything

--- displayEverything.hpp
+++ displayEverything.hpp
@@ -21,10 +21,45 @@ class DisplayEverything: public Display{
 			IndexItem* result;
 			Factory* factory = new Factory();
 	    		if(major.compare("Computer Science")==0){
-				result = new Major("CS","Computer Science");	
+				result = new Major("ENCS","Computer Science");	
 				result = factory->parse("dummy1.txt");
 				result->print();
 			}
+			else if(major.compare("Computer Engineering") == 0) {
+				result = new Major("ENCE","Computer Engineering");
+				result = factory->parse("ComputerEngineering.txt");
+				result->print();
+			}
+			else if(major.compare("Bioengineering") == 0) {
+				result = new Major("BIEN","Bioengineering");
+				result = factory->parse("Bioengineering.txt");
+				result->print();
+			}
+			else if(major.compare("Chemical Engineering") == 0) {
+				result = new Major("CHEN","Chemical Engineering");
+				result = factory->parse("ChemicalEngineering.txt");
+				result->print();
+			}
+			else if(major.compare("Data Science") == 0) {
+				result = new Major("ENDS","Data Science");
+				result = factory->parse("DataScience.txt");
+				result->print();
+			}
+			else if(major.compare("Electrical Engineering") == 0) {
+				result = new Major("EE","Electrical Engineering");
+				result = factory->parse("ElectricalEngineering.txt");
+				result->print();
+			}
+			else if(major.compare("Materials Science") == 0) {
+				result = new Major("MS","Materials Science");
+				result = factory->parse("MaterialScience.txt");
+				result->print();
+			}
+			else if(major.comapre("Mechanical Engineering") == 0) {
+				result = new Major("ME","Mechanical Engineering");
+				result = factory->parse("MechanicalEngineering.txt");
+				result->print();
+			}			
 			else{
 				cout<<"incorrect";
 			}
